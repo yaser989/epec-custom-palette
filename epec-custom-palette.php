053d1776b334ad643067cad71ca0569f628532b1
yaser <abbasyaser989@gmail.com>
2019-08-01 15:19:22 +0200

    test

--- epec-custom-palette.php
+++ epec-custom-palette.php
@@ -1,43 +1,41 @@
 <?php
 /*
 Plugin Name: Custom Colors Palette by Promo EPEC
-Plugin URI: https://github.com/yaser989/epec-custom-palette.git
+Plugin URI:https://github.com/yaser989/epec-custom-palette.git
 Description: Un plugin d'introduction pour le développement sous WordPress
 Version: 1.0
-Author: Yaser
+Author: yaser
 Author URI: http://votre-site.com
 License: GPL2
 */
 function mytheme_setup_theme_supported_features() {
    add_theme_support( 'editor-color-palette', array(
        array(
-           'name' => __( 'strong-magenta', 'epec-custom-palette' ),
-           'slug' => 'strong-magenta',
+           'name' => __( 'brand-color-1', 'epec-custom-palette' ),
+           'slug' => 'brand-color-1',
            'color' => '#6128A1
 ',
        ),
        array(
-           'name' => __( 'light-grayish-magenta', 'epec-custom-palette' ),
-           'slug' => 'light-grayish-magenta',
+           'name' => __( 'brand-Color-2', 'epec-custom-palette' ),
+           'slug' => 'brand-color-2',
            'color' => '#E94B59
 ',
        ),
        array(
-           'name' => __( 'Very Dark Grey', 'epec-custom-palette' ),
-           'slug' => 'very-light-gray',
+           'name' => __( 'Very-Dark-Grey', 'epec-custom-palette' ),
+           'slug' => 'very-dark-gray',
            'color' => '#111111
 ',
        ),
        array(
            'name' => __( 'white', 'epec-custom-palette' ),
-           'slug' => 'very-dark-gray',
+           'slug' => 'white',
            'color' => '#FFFFFF
 ',
        ),
    ) );
-}add_action( 'after_setup_theme', 'mytheme_setup_theme_supported_features' );
-
-function my_scripts() {
+}add_action( 'after_setup_theme', 'mytheme_setup_theme_supported_features' );function my_scripts() {
 wp_register_style( 'prefix-style', plugins_url('style.css', __FILE__) );
 wp_enqueue_style( 'prefix-style' );
 }
